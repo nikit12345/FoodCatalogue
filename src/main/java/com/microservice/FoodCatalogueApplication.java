59031b90ead006211f218f0f3e8c91fe168c77c9
avinash <nikitamane2000@gmail.com>
2025-03-05 12:11:47 +0530

    xml

--- src/main/java/com/microservice/FoodCatalogueApplication.java
+++ src/main/java/com/microservice/FoodCatalogueApplication.java
@@ -2,8 +2,12 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.blocking.XForwardedHeadersTransformer;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,5 +23,11 @@ public class FoodCatalogueApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-	
+	@Bean
+	@ConditionalOnMissingBean(XForwardedHeadersTransformer.class)
+	@ConditionalOnBean(LoadBalancerClientFactory.class)
+	public XForwardedHeadersTransformer xForwarderHeadersTransformer(
+			LoadBalancerClientFactory loadBalancerClientFactory) {
+		return new XForwardedHeadersTransformer(loadBalancerClientFactory);
+	}
 }
